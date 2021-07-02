bf7dc45c689297c0edf5891c602bbcc8859c3397
Harrison-O <mr.outram@protonmail.com>
2021-07-02 23:15:06 +0800

    Added ignore optional parameter to converter

--- src/attendees/attendee_converter.py
+++ src/attendees/attendee_converter.py
@@ -12,19 +12,23 @@ import pandas as pd
 
 from .attendee_manager import Attendee, AttendeeManager
 
-def pandas2manager(attendees: pd.DataFrame) -> AttendeeManager:
+def pandas2manager(attendees: pd.DataFrame, ignore: set = None
+                  ) -> AttendeeManager:
     """
     Convert Pandas DataFrame attendee record to AttendeeManager.
 
     Args:
         attendees (pd.DataFrame): The attendees as a Pandas DataFrame.
+        ignore (set): Collection of attributes to ignore, if left as None
+        will not ignore any fields.
 
     Returns:
         AttendeeManager: The attendees as a AttendeeManager.
     """
     raise NotImplementedError
 
-def manager2pandas(attendees: AttendeeManager) -> pd.DataFrame:
+def manager2pandas(attendees: AttendeeManager, ignore: set = None
+                  ) -> pd.DataFrame:
     """
     TODO: Write docstring
     """

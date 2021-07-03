7666c1916e0c31847dc2fccb24e5db6ad3788bd4
Harrison-O <mr.outram@protonmail.com>
2021-07-03 15:01:45 +0800

    Wrote root test driver

---
+++ test_driver.py
@@ -0,0 +1,33 @@
+"""
+Test driver script for all test scripts.
+
+Required since Python does not allow imports above top level hierarchy.
+Running this script forces the top level hierarchy to be the root of
+this project.
+"""
+
+import sys
+import argparse
+from typing import Dict
+
+import pytest
+
+def main():
+    argv = process_argv()
+    test_path = argv["path"]
+    sys.exit(pytest([test_path]))
+
+def process_argv() -> Dict[str, str]:
+    if len(sys.argv) == 1:
+        print("Must run test driver with exactly one command line argument",
+                file=sys.stderr)
+        print("Use -h or --help for help", file=sys.stderr)
+        sys.exit(1)
+
+    parser = argparse.ArgumentParser("Certificate Automater Test Driver")
+    parser.add_argument("path")
+    argv = parser.parse_args(sys.argv[1:])
+    return vars(argv)
+
+if __name__ == "__main__":
+    main()

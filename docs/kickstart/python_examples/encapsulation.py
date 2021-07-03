55be0135d0d6dfe837edc130aa52604a8172ab65
Harrison-O <mr.outram@protonmail.com>
2021-07-04 00:29:13 +0800

    2nd draft kickstart doc

--- docs/kickstart/python_examples/encapsulation.py
+++ docs/kickstart/python_examples/encapsulation.py
@@ -1,20 +1,21 @@
 class MarbleJar:
-    def __init__(self, material, capacity, num_marbles = 0):
+    def __init__(self, material: str, capacity: int,
+                 num_marbles: int = 0):
         self._material = material
         self._capacity = capacity
         self._num_marbles = num_marbles
     
     @property
-    def material(self): return self._material
+    def material(self) -> str: return self._material
 
     @property
-    def capacity(self): return self._capacity
+    def capacity(self) -> int: return self._capacity
 
     @property
-    def num_marbles(self): return self._num_marbles
+    def num_marbles(self) -> int: return self._num_marbles
 
     @num_marbles.setter
-    def num_marbles(self, new_num_marbles):
+    def num_marbles(self, new_num_marbles: int):
         if new_num_marbles < 0:
             raise ValueError("Number of marbles cannot be less than 0")
         

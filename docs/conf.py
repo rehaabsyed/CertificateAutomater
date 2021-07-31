4f26c87aeeabd910a1db10cb7a93eac3562782cb
Harrison-O <mr.outram@protonmail.com>
2021-07-31 17:56:22 +0800

    Updated sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -70,3 +70,14 @@ html_use_smartypants = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# autodoc settings
+autodoc_type_aliases = {
+    "BatchStatusFunc": "BatchStatusFunc",
+}
+
+autodoc_default_options = {
+    "member-order": "bysource",
+    "special-members": "__init__"
+}

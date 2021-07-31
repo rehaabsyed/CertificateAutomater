4957ddd4fb508316b0d15900a1f3d00ac3538ec4
Harrison-O <mr.outram@protonmail.com>
2021-07-31 18:08:17 +0800

    Wrote type alias for certificate status func

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,8 @@ html_static_path = ['_static']
 # autodoc settings
 autodoc_type_aliases = {
     "BatchStatusFunc": "BatchStatusFunc",
-    "NamingFunc": "NamingFunc"
+    "NamingFunc": "NamingFunc",
+    "CertStatusFunc": "CertStatusFunc"
 }
 
 autodoc_default_options = {
--- src/certificate_creator/certificate_maker.py
+++ src/certificate_creator/certificate_maker.py
@@ -27,9 +27,17 @@ Return:
     str: The generated name.
 """
 
+CertStatusFunc = Callable[[Attendee, Exception], None]
+"""Alias for certificate status function.
+
+Args:
+    attendee (Attendee): The attendee whose certificate has been processed.
+    err (Exception): The error that occurred. ``None`` if successful.
+"""
+
 def createCertificate(in_path: str, out_path: str, attendees: AttendeeManager,
                      namingFunc: NamingFunc = None,
-                     statusFunc: Callable[[Attendee, Exception], None] = None,
+                     statusFunc: CertStatusFunc = None,
                      overwrite: bool = True) -> AttendeeManager:
     """
     Creates and saves certificates given template and the attendee record.
@@ -39,11 +47,11 @@ def createCertificate(in_path: str, out_path: str, attendees: AttendeeManager,
         out_dir (str): The folder to save the certificate to.
         attendees (AttendeeManager): The collection of attendees.
         namingFunc (NamingFunc): What to name the certificate, excluding the file extension. Defaults to ``None``, meaning certificates are automatically named.
-        statusFunc (Callable[[Attendee, bool], None]): Status update function to inform caller of what certificates have been made. Must take in ``Attendee`` object and whether it was successfully created (bool). Useful for user interfaces. If left as ``None``, does nothing.
-        overwrite (bool): Whether to overwrite certificate file that already exist, defaults to ``True``. Should call statusFunc with ``FileExistsError`` if False.
+        statusFunc (CertStatusFunc): Status update function to inform caller of what certificates have been made. Must take in ``Attendee`` object, then the error or ``None`` if no error occurred. Defaults to ``None``, meaning no reporting is done.
+        overwrite (bool): Whether to overwrite certificate file that already exist, defaults to ``True``. Should call ``statusFunc`` with ``FileExistsError`` if ``False``.
 
     Returns:
-        Updated attendees with "cert_path" field. Should map to ``None`` if failed
+        Updated attendees with ``"cert_path"`` field. Should map to ``None`` if failed
         to create certificate.
 
     Raises:
